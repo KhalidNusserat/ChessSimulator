2ddc698172af76f68d9c2c89cdb65ed06dcd388f
KhalidNusserat <KhMKhNu@gmail.com>
2022-04-13 14:36:46 +0300

    used String.matches() instead of Pattern and Matcher

--- src/main/java/com/atypon/chessgame/BoardPosition.java
+++ src/main/java/com/atypon/chessgame/BoardPosition.java
@@ -1,21 +1,15 @@
 package com.atypon.chessgame;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public record BoardPosition(String position) {
-    public static BoardPosition at(String position) {
-        return new BoardPosition(position);
-    }
-
     public BoardPosition(String position) {
-        Pattern chessBoardPositionPattern = Pattern.compile("^[a-hA-H][1-8]$");
-        Matcher matcher = chessBoardPositionPattern.matcher(position);
-        if (matcher.matches()) {
+        if (position.matches("^[a-hA-H][1-8]$")) {
             this.position = position;
-        }
-        else {
+        } else {
             throw new IllegalArgumentException(position + " is not a valid chess board position.");
         }
+    }
+
+    public static BoardPosition at(String position) {
+        return new BoardPosition(position);
     }
 }
