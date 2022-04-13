c26f0c4f5ac8f4a1edf95ee63d51f3b0f74f1d6c
KhalidNusserat <KhMKhNu@gmail.com>
2022-04-13 15:00:43 +0300

    created DefaultMoveParser and test

---
+++ src/main/java/com/atypon/chessgame/DefaultMoveParser.java
@@ -0,0 +1,24 @@
+package com.atypon.chessgame;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class DefaultMoveParser implements MoveParser {
+    private static final String pattern = "^move +([a-hA-H][1-8]) +([a-hA-H][1-8]) *$";
+
+    @Override
+    public Move parseMove(String move, ChessColor playerColor) {
+        Pattern startWithMove = Pattern.compile(pattern);
+        Matcher matcher = startWithMove.matcher(move);
+        if (matcher.matches()) {
+            String from = matcher.group(1);
+            String to = matcher.group(2);
+            return new Move(
+                    playerColor,
+                    BoardPosition.at(from),
+                    BoardPosition.at(to)
+            );
+        }
+        throw new IllegalArgumentException(move + " is not a recognised chess command");
+    }
+}
---
+++ src/test/java/com/atypon/chessgame/DefaultMoveParserTest.java
@@ -0,0 +1,37 @@
+package com.atypon.chessgame;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class DefaultMoveParserTest {
+    @Test
+    void parsing() {
+        DefaultMoveParser parser = new DefaultMoveParser();
+        assertEquals(
+                parser.parseMove("move A1 B2", ChessColor.WHITE),
+                new Move(ChessColor.WHITE, BoardPosition.at("A1"), BoardPosition.at("B2"))
+        );
+        assertEquals(
+                parser.parseMove("move  B1  C4 ", ChessColor.BLACK),
+                new Move(ChessColor.BLACK, BoardPosition.at("B1"), BoardPosition.at("C4"))
+        );
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> parser.parseMove("mv a1 b2", ChessColor.BLACK)
+        );
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> parser.parseMove("move a1 b2 a3", ChessColor.BLACK)
+        );
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> parser.parseMove("move a1 ", ChessColor.BLACK)
+        );
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> parser.parseMove("move a1 j2", ChessColor.BLACK)
+        );
+    }
+}
