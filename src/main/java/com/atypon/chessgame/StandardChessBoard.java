b66b4042ea9f5525225189e80fdb75403b15a8b3
KhalidNusserat <KhMKhNu@gmail.com>
2022-04-13 14:20:52 +0300

    created StandardChessBoard and tested it

---
+++ src/main/java/com/atypon/chessgame/StandardChessBoard.java
@@ -0,0 +1,24 @@
+package com.atypon.chessgame;
+
+public class StandardChessBoard extends BoardState {
+    private void initializePlayer(ChessColor color, String pawnsLevel, String otherPiecesLevel) {
+        for (int i = 0; i < 8; i++) {
+            board.put(
+                    BoardPosition.at((char) ('A' + i) + pawnsLevel),
+                    new ChessPiece(ChessPieceType.PAWN, color)
+            );
+        }
+        board.put(BoardPosition.at("A" + otherPiecesLevel), new ChessPiece(ChessPieceType.ROOK, color));
+        board.put(BoardPosition.at("H" + otherPiecesLevel), new ChessPiece(ChessPieceType.ROOK, color));
+        board.put(BoardPosition.at("B" + otherPiecesLevel), new ChessPiece(ChessPieceType.KNIGHT, color));
+        board.put(BoardPosition.at("G" + otherPiecesLevel), new ChessPiece(ChessPieceType.KNIGHT, color));
+        board.put(BoardPosition.at("C" + otherPiecesLevel), new ChessPiece(ChessPieceType.BISHOP, color));
+        board.put(BoardPosition.at("F" + otherPiecesLevel), new ChessPiece(ChessPieceType.BISHOP, color));
+        board.put(BoardPosition.at("D" + otherPiecesLevel), new ChessPiece(ChessPieceType.QUEEN, color));
+        board.put(BoardPosition.at("E" + otherPiecesLevel), new ChessPiece(ChessPieceType.KING, color));
+    }
+    public StandardChessBoard() {
+        initializePlayer(ChessColor.WHITE, "2", "1");
+        initializePlayer(ChessColor.BLACK, "7", "8");
+    }
+}
---
+++ src/test/java/com/atypon/chessgame/StandardBoardTest.java
@@ -0,0 +1,22 @@
+package com.atypon.chessgame;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StandardBoardTest {
+    @Test
+    void testInitialBoard() {
+        StandardChessBoard chessBoard = new StandardChessBoard();
+        for (int i = 0; i < 8; i++) {
+            assertEquals(
+                    chessBoard.getPieceAt(BoardPosition.at((char) ('A' + i) + "2")),
+                    new ChessPiece(ChessPieceType.PAWN, ChessColor.WHITE)
+            );
+        }
+        assertEquals(
+                chessBoard.getPieceAt(BoardPosition.at("A1")),
+                new ChessPiece(ChessPieceType.ROOK, ChessColor.WHITE)
+        );
+    }
+}
