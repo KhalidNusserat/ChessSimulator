8c7cbab369bdb485fcfa960debf5361de95f5a54
KhalidNusserat <KhMKhNu@gmail.com>
2022-04-13 14:19:59 +0300

    created MoveChecker

---
+++ src/main/java/com/atypon/chessgame/MoveChecker.java
@@ -0,0 +1,9 @@
+package com.atypon.chessgame;
+
+import java.util.Collection;
+
+public interface MoveChecker {
+    void addRule(MoveRule moveRule);
+    void addAllRules(Collection<MoveRule> moveRules);
+    boolean checkMove(Move move, ChessGameModel defaultChessGameModel);
+}
