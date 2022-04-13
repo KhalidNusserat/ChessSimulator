6337f503fba1b6781ac9c79682a12a6b0939cc42
KhalidNusserat <KhMKhNu@gmail.com>
2022-04-13 14:10:20 +0300

    created BoardState

---
+++ src/main/java/com/atypon/chessgame/BoardState.java
@@ -0,0 +1,12 @@
+package com.atypon.chessgame;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BoardState {
+    protected final Map<BoardPosition, ChessPiece> board = new HashMap<>();
+
+    public ChessPiece getPieceAt(BoardPosition position) {
+        return board.get(position);
+    }
+}
