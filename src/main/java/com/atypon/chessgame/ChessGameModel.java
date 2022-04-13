36921ad47bbcd3991265cd0557abab9da9765250
KhalidNusserat <KhMKhNu@gmail.com>
2022-04-13 14:08:43 +0300

    changed Player array to a Map<ChessColor, Player>

--- src/main/java/com/atypon/chessgame/ChessGameModel.java
+++ src/main/java/com/atypon/chessgame/ChessGameModel.java
@@ -2,60 +2,24 @@ package com.atypon.chessgame;
 
 import com.atypon.chessgame.utils.CachedLinkedHashSet;
 
-public class ChessGameModel {
-    private final Player[] players;
+import java.util.Map;
 
-    private final CachedLinkedHashSet<BoardState> boardStates;
+public interface ChessGameModel {
+    void changeTurns();
 
-    private Player currentPlayer;
+    BoardState getCurrentState();
 
-    private Player winner;
+    CachedLinkedHashSet<BoardState> getBoardStates();
 
-    private boolean isDone;
+    boolean isDone();
 
-    public ChessGameModel(String whitePlayer, String blackPlayer) {
-        players = new Player[]{
-                new Player(whitePlayer, ChessColor.WHITE),
-                new Player(blackPlayer, ChessColor.BLACK)
-        };
-        currentPlayer = players[0];
-        boardStates = new CachedLinkedHashSet<>();
-        boardStates.add(new BoardState());
-    }
+    Player getWinner();
 
-    public void changeTurns() {
-        currentPlayer = (currentPlayer == players[0] ? players[1] : players[0]);
-    }
+    Map<ChessColor, Player> getPlayers();
 
-    public BoardState getCurrentState() {
-        return boardStates.getLast();
-    }
+    Player getCurrentPlayer();
 
-    public CachedLinkedHashSet<BoardState> getBoardStates() {
-        return boardStates;
-    }
+    void setWinner(Player winner);
 
-    public boolean isDone() {
-        return isDone;
-    }
-
-    public Player getWinner() {
-        return winner;
-    }
-
-    public Player[] getPlayers() {
-        return players;
-    }
-
-    public Player getCurrentPlayer() {
-        return currentPlayer;
-    }
-
-    public void setWinner(Player winner) {
-        this.winner = winner;
-    }
-
-    public void setDone(boolean done) {
-        isDone = done;
-    }
+    void setDone(boolean done);
 }
