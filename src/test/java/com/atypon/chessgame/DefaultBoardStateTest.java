353ef48ba00ca5f85b750e8ab1640f37ef8b4e45
KhalidNusserat <KhMKhNu@gmail.com>
2022-04-14 16:53:59 +0300

    renamed StandardChessBoard to DefaultBoardState

--- src/test/java/com/atypon/chessgame/DefaultBoardStateTest.java
+++ src/test/java/com/atypon/chessgame/DefaultBoardStateTest.java
@@ -7,10 +7,10 @@ import java.util.List;
 
 import static org.junit.Assert.*;
 
-public class StandardBoardTest {
+public class DefaultBoardStateTest {
     @Test
     public void testInitialBoard() {
-        DefaultBoardState chessBoard = new DefaultBoardState();
+        com.atypon.chessgame.model.DefaultBoardState chessBoard = new com.atypon.chessgame.model.DefaultBoardState();
         for (int i = 0; i < 8; i++) {
             assertEquals(
                     chessBoard.getPieceAt(BoardPosition.at((char) ('A' + i) + "2")),
@@ -25,35 +25,35 @@ public class StandardBoardTest {
 
     @Test
     public void testWithSwapped() {
-        DefaultBoardState chessBoard = new DefaultBoardState();
-        chessBoard = (DefaultBoardState) chessBoard.withSwapped(BoardPosition.at("A2"), BoardPosition.at("A4"));
+        com.atypon.chessgame.model.DefaultBoardState chessBoard = new com.atypon.chessgame.model.DefaultBoardState();
+        chessBoard = (com.atypon.chessgame.model.DefaultBoardState) chessBoard.withSwapped(BoardPosition.at("A2"), BoardPosition.at("A4"));
         assertNull(chessBoard.getPieceAt(BoardPosition.at("A2")));
         assertEquals(chessBoard.getPieceAt(BoardPosition.at("A4")).type(), ChessPieceType.PAWN);
-        chessBoard = (DefaultBoardState) chessBoard.withSwapped(BoardPosition.at("A3"), BoardPosition.at("A2"));
+        chessBoard = (com.atypon.chessgame.model.DefaultBoardState) chessBoard.withSwapped(BoardPosition.at("A3"), BoardPosition.at("A2"));
         assertNull(chessBoard.getPieceAt(BoardPosition.at("A3")));
         assertNull(chessBoard.getPieceAt(BoardPosition.at("A2")));
-        chessBoard = (DefaultBoardState) chessBoard.withSwapped(BoardPosition.at("A4"), BoardPosition.at("H1"));
+        chessBoard = (com.atypon.chessgame.model.DefaultBoardState) chessBoard.withSwapped(BoardPosition.at("A4"), BoardPosition.at("H1"));
         assertEquals(chessBoard.getPieceAt(BoardPosition.at("A4")).type(), ChessPieceType.ROOK);
         assertEquals(chessBoard.getPieceAt(BoardPosition.at("H1")).type(), ChessPieceType.PAWN);
     }
 
     @Test
     public void testWithout() {
-        DefaultBoardState chessBoard = new DefaultBoardState();
-        chessBoard = (DefaultBoardState) chessBoard.without(BoardPosition.at("E8"));
+        com.atypon.chessgame.model.DefaultBoardState chessBoard = new com.atypon.chessgame.model.DefaultBoardState();
+        chessBoard = (com.atypon.chessgame.model.DefaultBoardState) chessBoard.without(BoardPosition.at("E8"));
         assertNull(chessBoard.getPieceAt(BoardPosition.at("E8")));
     }
 
     @Test
     public void testGetPositionOfType() {
-        DefaultBoardState chessBoard = new DefaultBoardState();
+        com.atypon.chessgame.model.DefaultBoardState chessBoard = new com.atypon.chessgame.model.DefaultBoardState();
         List<BoardPosition> positionsOfPawns = chessBoard.getPositionsOf(ChessPieceType.PAWN);
         assertEquals(positionsOfPawns.size(), 16);
     }
 
     @Test
     public void testGetPositionsOfPiece() {
-        DefaultBoardState chessBoard = new DefaultBoardState();
+        com.atypon.chessgame.model.DefaultBoardState chessBoard = new com.atypon.chessgame.model.DefaultBoardState();
         List<BoardPosition> positionsOfBlackRooks = chessBoard.getPositionsOf(
                 new ChessPiece(ChessPieceType.ROOK, ChessColor.BLACK)
         );
@@ -64,29 +64,29 @@ public class StandardBoardTest {
 
     @Test
     public void testContainsType() {
-        DefaultBoardState chessBoard = new DefaultBoardState();
+        com.atypon.chessgame.model.DefaultBoardState chessBoard = new com.atypon.chessgame.model.DefaultBoardState();
         assertTrue(chessBoard.contains(ChessPieceType.PAWN));
         List<BoardPosition> positionsOfPawns = chessBoard.getPositionsOf(ChessPieceType.PAWN);
         for (BoardPosition position : positionsOfPawns) {
-            chessBoard = (DefaultBoardState) chessBoard.without(position);
+            chessBoard = (com.atypon.chessgame.model.DefaultBoardState) chessBoard.without(position);
         }
         assertFalse(chessBoard.contains(ChessPieceType.PAWN));
     }
 
     @Test
     public void testContainsPiece() {
-        DefaultBoardState chessBoard = new DefaultBoardState();
+        com.atypon.chessgame.model.DefaultBoardState chessBoard = new com.atypon.chessgame.model.DefaultBoardState();
         ChessPiece whiteKing = new ChessPiece(ChessPieceType.KING, ChessColor.WHITE);
         assertTrue(chessBoard.contains(whiteKing));
-        chessBoard = (DefaultBoardState) chessBoard.without(chessBoard.getPositionsOf(whiteKing).get(0));
+        chessBoard = (com.atypon.chessgame.model.DefaultBoardState) chessBoard.without(chessBoard.getPositionsOf(whiteKing).get(0));
         assertFalse(chessBoard.contains(whiteKing));
     }
 
     @Test
     public void testWithPieceAt() {
-        DefaultBoardState chessBoard = new DefaultBoardState();
+        com.atypon.chessgame.model.DefaultBoardState chessBoard = new com.atypon.chessgame.model.DefaultBoardState();
         ChessPiece newWhiteKing = new ChessPiece(ChessPieceType.KING, ChessColor.BLACK);
-        chessBoard = (DefaultBoardState) chessBoard.withPieceAt(newWhiteKing, BoardPosition.at("D5"));
+        chessBoard = (com.atypon.chessgame.model.DefaultBoardState) chessBoard.withPieceAt(newWhiteKing, BoardPosition.at("D5"));
         assertEquals(chessBoard.getPieceAt(BoardPosition.at("D5")), newWhiteKing);
     }
 }
