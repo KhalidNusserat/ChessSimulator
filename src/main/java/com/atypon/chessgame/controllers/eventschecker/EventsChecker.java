50cd0865f6e6d4b99baca7148f60fd6c0f8e009f
KhalidNusserat <KhMKhNu@gmail.com>
2022-04-14 15:49:40 +0300

    added add methods to EventsChecker

--- src/main/java/com/atypon/chessgame/controllers/eventschecker/EventsChecker.java
+++ src/main/java/com/atypon/chessgame/controllers/eventschecker/EventsChecker.java
@@ -2,8 +2,15 @@ package com.atypon.chessgame.controllers.eventschecker;
 
 import com.atypon.chessgame.model.ChessGameModel;
 
+import java.util.Collection;
 import java.util.List;
 
 public interface EventsChecker {
+    void addEventChecker(EventChecker eventChecker);
+
+    void addAllEventCheckers(Collection<EventChecker> eventsCheckers);
+
+    void addAllEventCheckers(EventChecker... eventCheckers);
+
     List<ChessEvent> getEvents(ChessGameModel chessGameModel);
 }
