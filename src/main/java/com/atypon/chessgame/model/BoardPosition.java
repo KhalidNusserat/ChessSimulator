990dfef0f8e616692342d5825132d4c6bad3b355
KhalidNusserat <KhMKhNu@gmail.com>
2022-04-14 13:55:50 +0300

    made BoardPosition checks for validity

--- src/main/java/com/atypon/chessgame/model/BoardPosition.java
+++ src/main/java/com/atypon/chessgame/model/BoardPosition.java
@@ -1,6 +1,15 @@
 package com.atypon.chessgame.model;
 
 public record BoardPosition(String position) {
+    public BoardPosition(String position) {
+        if (position.matches("^[a-hA-H][1-8]$")) {
+            this.position = position;
+        }
+        else {
+            throw new IllegalArgumentException(position + " is not a valid chess position.");
+        }
+    }
+
     public static BoardPosition at(String position) {
         return new BoardPosition(position);
     }
---
+++ src/test/java/com/atypon/chessgame/BoardPositionTest.java
@@ -0,0 +1,27 @@
+package com.atypon.chessgame;
+
+import com.atypon.chessgame.model.BoardPosition;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class BoardPositionTest {
+    @Test
+    public void shouldValidatePosition() {
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> BoardPosition.at("A12")
+        );
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> BoardPosition.at("I1")
+        );
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> BoardPosition.at("A 1")
+        );
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> BoardPosition.at(" a1")
+        );
+    }
+}
