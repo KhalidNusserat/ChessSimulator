4d5295514ba7c7be3cb451ec3b3e5550983a0ed6
KhalidNusserat <KhMKhNu@gmail.com>
2022-04-15 15:50:13 +0300

    PawnPromotionEvent now stores information about the promoted pawn

--- src/main/java/com/atypon/chessgame/controllers/eventschecker/PawnPromotionEvent.java
+++ src/main/java/com/atypon/chessgame/controllers/eventschecker/PawnPromotionEvent.java
@@ -1,4 +1,15 @@
 package com.atypon.chessgame.controllers.eventschecker;
 
+import com.atypon.chessgame.model.BoardPosition;
+
 public class PawnPromotionEvent implements ChessEvent {
+    private final BoardPosition pawnPosition;
+
+    public PawnPromotionEvent(BoardPosition pawnPosition) {
+        this.pawnPosition = pawnPosition;
+    }
+
+    public BoardPosition getPawnPosition() {
+        return pawnPosition;
+    }
 }
