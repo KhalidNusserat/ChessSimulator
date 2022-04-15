3633011eb304d9655518a706b239fe3e9114187f
KhalidNusserat <KhMKhNu@gmail.com>
2022-04-15 14:21:53 +0300

    added getBlack() and getWhite() for ChessPiece

--- src/main/java/com/atypon/chessgame/model/ChessPiece.java
+++ src/main/java/com/atypon/chessgame/model/ChessPiece.java
@@ -1,4 +1,11 @@
 package com.atypon.chessgame.model;
 
 public record ChessPiece(ChessPieceType type, ChessColor color) {
+    public static ChessPiece getWhite(ChessPieceType type) {
+        return new ChessPiece(type, ChessColor.WHITE);
+    }
+
+    public static ChessPiece getBlack(ChessPieceType type) {
+        return new ChessPiece(type, ChessColor.BLACK);
+    }
 }
