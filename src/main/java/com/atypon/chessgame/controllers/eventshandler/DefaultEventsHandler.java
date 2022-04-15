a29c5ff53fcc93f07a4ce452d8649b2ab02a1dd3
KhalidNusserat <KhMKhNu@gmail.com>
2022-04-15 16:56:08 +0300

    created DefaultEventsHandler

---
+++ src/main/java/com/atypon/chessgame/controllers/eventshandler/DefaultEventsHandler.java
@@ -0,0 +1,44 @@
+package com.atypon.chessgame.controllers.eventshandler;
+
+import com.atypon.chessgame.controllers.eventschecker.ChessEvent;
+import com.atypon.chessgame.model.ChessGameModel;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class DefaultEventsHandler implements EventsHandler {
+    private final Map<Class<? extends ChessEvent>, EventHandler> eventHandlers = new HashMap<>();
+
+    @Override
+    public <E extends ChessEvent> void addEventHandler(Class<E> eventClass, EventHandler eventHandler) {
+        eventHandlers.put(eventClass, eventHandler);
+    }
+
+    @Override
+    public <E extends ChessEvent> void addAllEventHandlers(Class<E> eventClass, Collection<EventHandler> eventHandlers) {
+        for (EventHandler eventHandler : eventHandlers) {
+            this.eventHandlers.put(eventClass, eventHandler);
+        }
+    }
+
+    @Override
+    public <E extends ChessEvent> void addAllEventHandlers(Class<E> eventClass, EventHandler... eventHandlers) {
+        for (EventHandler eventHandler : eventHandlers) {
+            this.eventHandlers.put(eventClass, eventHandler);
+        }
+    }
+
+    @Override
+    public void handleEvent(ChessEvent chessEvent, ChessGameModel chessGameModel) {
+        eventHandlers.get(chessEvent.getClass()).handle(chessEvent, chessGameModel);
+    }
+
+    @Override
+    public void handleEvents(List<ChessEvent> chessEvents, ChessGameModel chessGameModel) {
+        for (ChessEvent chessEvent : chessEvents) {
+            eventHandlers.get(chessEvent.getClass()).handle(chessEvent, chessGameModel);
+        }
+    }
+}
