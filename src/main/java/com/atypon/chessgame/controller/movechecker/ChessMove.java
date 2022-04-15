ad23f2427c5025b8d9ba9d3c50e42f7fd7c39b43
KhalidNusserat <KhMKhNu@gmail.com>
2022-04-15 18:50:10 +0300

    overridden toString in ChessMove

--- src/main/java/com/atypon/chessgame/controller/movechecker/ChessMove.java
+++ src/main/java/com/atypon/chessgame/controller/movechecker/ChessMove.java
@@ -4,4 +4,8 @@ import com.atypon.chessgame.model.BoardPosition;
 import com.atypon.chessgame.model.ChessColor;
 
 public record ChessMove(ChessColor playerColor, BoardPosition from, BoardPosition to) {
+    @Override
+    public String toString() {
+        return String.format("move %s %s", from, to);
+    }
 }
