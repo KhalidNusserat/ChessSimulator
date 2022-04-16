4b2c0c7af982fac6d192802b3e5151cdc3685962
KhalidNusserat <KhMKhNu@gmail.com>
2022-04-16 12:18:20 +0300

    added ChessPiece.toString

--- src/main/java/com/atypon/chessgame/model/ChessPiece.java
+++ src/main/java/com/atypon/chessgame/model/ChessPiece.java
@@ -8,4 +8,19 @@ public record ChessPiece(ChessPieceType type, ChessColor color) {
     public static ChessPiece getBlack(ChessPieceType type) {
         return new ChessPiece(type, ChessColor.BLACK);
     }
+
+    @Override
+    public String toString() {
+        String pieceColor = (color == ChessColor.WHITE ? "W" : "B");
+        String pieceType = "";
+        switch (type) {
+            case KING -> pieceType = "K";
+            case PAWN -> pieceType = "P";
+            case ROOK -> pieceType = "R";
+            case QUEEN -> pieceType = "Q";
+            case BISHOP -> pieceType = "B";
+            case KNIGHT -> pieceType = "N";
+        }
+        return pieceColor + pieceType;
+    }
 }
