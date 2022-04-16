ff711799f0bfa195459690f9df7b54472514bca2
KhalidNusserat <KhMKhNu@gmail.com>
2022-04-16 15:16:51 +0300

    BoardPosition now doesn't check for validity

--- src/main/java/com/atypon/chessgame/model/BoardPosition.java
+++ src/main/java/com/atypon/chessgame/model/BoardPosition.java
@@ -2,17 +2,7 @@ package com.atypon.chessgame.model;
 
 import java.util.Objects;
 
-public class BoardPosition {
-    private final String position;
-
-    public BoardPosition(String position) {
-        if (position.matches("^[a-hA-H][1-8]$")) {
-            this.position = position;
-        } else {
-            throw new IllegalArgumentException(position + " is not a valid chess position.");
-        }
-    }
-
+public record BoardPosition(String position) {
     public static BoardPosition at(String position) {
         return new BoardPosition(position);
     }
