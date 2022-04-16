58c909863b3c31cab0910a9e17d2dc5c6ffac66e
KhalidNusserat <KhMKhNu@gmail.com>
2022-04-16 18:03:15 +0300

    turned CheckMateEvent into a record

--- src/main/java/com/atypon/chessgame/controller/eventschecker/CheckMateEvent.java
+++ src/main/java/com/atypon/chessgame/controller/eventschecker/CheckMateEvent.java
@@ -2,13 +2,7 @@ package com.atypon.chessgame.controller.eventschecker;
 
 import com.atypon.chessgame.model.ChessColor;
 
-public class CheckMateEvent implements ChessEvent {
-    private final ChessColor winner;
-
-    public CheckMateEvent(ChessColor winner) {
-        this.winner = winner;
-    }
-
+public record CheckMateEvent(ChessColor winner) implements ChessEvent {
     public ChessColor getWinnerColor() {
         return winner;
     }
